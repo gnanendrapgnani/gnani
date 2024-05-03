8e6d1808133f079a940f60b4eef858f2d84a544a
Gnanendra P <131912364+gnanendrapgnani@users.noreply.github.com>
2024-05-03 20:10:05 +0530

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,31 @@
+let loader=document.getElementById('preloader');
+let icon=document.querySelector('.icon')
+
+
+
+icon.onclick=function(){
+    navBar=document.querySelector('.navContanier')
+    navBar.classList.toggle("active")
+}
+
+
+window.addEventListener("load", function(){
+    loader.style.display="none";  
+});
+
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+const hiddenElements=document.querySelectorAll('.hidden')
+hiddenElements.forEach((el)=>observer.observe(el))
